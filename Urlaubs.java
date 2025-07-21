1ae8db407286ca68b136e3c8a631a584f30c2306
CEC <learnmorececc1@gmail.com>
2025-07-21 19:34:32 +0200

    printUrlaubs Methode hinzugefügt

--- Urlaubs.java
+++ Urlaubs.java
@@ -41,4 +41,9 @@ public class Urlaubs
     {
         return all;
     }
+    
+    public void printUrlaubs()
+    {
+        System.out.println("Der Ort ist " + ort + " , es kostet " + preis + " Euro und der AllInklusive ist: " + all);
+    }
 }
